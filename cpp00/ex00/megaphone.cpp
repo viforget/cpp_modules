21770e98cca5739b9cb11df6b7f53134265363cf
viforget <victor.forget99@gmail.com>
2021-10-14 15:42:04 +0200

    first exercise

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			for (int j = 0; av[i][j]; j++)
+				av[i][j] = toupper(av[i][j]);	
+			std::cout << av[i];
+			if (i < ac - 1)
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+}
