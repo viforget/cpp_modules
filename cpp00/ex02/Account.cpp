2531e61876090200e00527945ca23b203c50eeb4
Victor Forget <viforget@pop-os.localdomain>
2021-10-20 17:42:24 +0200

    advance in ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -10,49 +10,64 @@ Account::Account( int deposit ) {
 	
 	this->_amount = deposit;
 	this->_accountIndex = Account::_nbAccounts;
-	std::cout << "index:" << Account::_nbAccounts << ";amount:" << deposit << ";created" << std::endl;
+	Account::_totalAmount += deposit;
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
 	Account::_nbAccounts += 1;
 	return;
 }
 
 Account::~Account( void ) {
 
+	std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 	return;
 }
-
-/*int	Account::getNbAccounts( void )
+/*
+int	Account::getNbAccounts( void )
 {
 	return (Account::_nbAccounts);
-}*/
+}
 
-/*int	getTotalAmount( void )
+int	Account::getTotalAmount( void )
 {
 	return (Account::_totalAmount);
 }
 
-int	getNbDeposits( void )
+int	Account::getNbDeposits( void )
 {
 	return (Account::_totalNbDeposits);
 }
 
-int	getNbWithdrawals( void )
+int	Account::getNbWithdrawals( void )
 {
 	return (Account::_totalNbWithdrawals);
 }
-
-void	displayAccountsInfos( void )
-{
-	return (Account::_nbAccounts);
-}*/
-
-void	Account::makeDeposit( int deposit) {
-	
-	//Account::_totalNbDeposits += 1;
-	//Account::_totalAmount += deposit;
-	this->_amount += deposit;
-}
-
+*/
 void	Account::displayAccountsInfos( void )
 {
 	std::cout << "accounts:" << Account::_nbAccounts << ";total:" << Account::_totalAmount << ";deposits:" << Account::_totalNbDeposits << ";withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+	Account::_totalAmount += deposit;
+	this->_amount += deposit;
+	return;
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	if (this->_amount < withdrawal)
+		return (false);
+	this->_nbWithdrawals ++;
+	Account::_totalNbWithdrawals++;
+	Account::_totalAmount -= withdrawal;
+	this->_amount -= withdrawal;
+	return (true);
+}
+
+void	Account::displayStatus( void ) const
+{
+		std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount << ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals << std::endl;
 }
