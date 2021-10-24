db085359149c98af3055318ec306832f595ba411
Victor Forget <viforget@pop-os.localdomain>
2021-10-24 12:30:36 +0200

    add ex05

---
+++ cpp01/ex05/Karen.hpp
@@ -0,0 +1,25 @@
+#ifndef KAREN_HPP
+# define KAREN_HPP
+
+#include <iostream>
+
+class Karen
+{
+
+public:
+	
+	Karen( void );
+	~Karen( void );
+
+	void complain( std::string level );
+
+private:
+	void debug( void );
+	void info( void );
+	void warning( void );
+	void error( void );
+	
+};
+
+
+#endif
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Karen.hpp"
+
+int main()
+{
+    Karen ppl;
+
+    ppl.complain("DEBUG");
+    ppl.complain("INFO");
+    ppl.complain("WARNING");
+    ppl.complain("ERROR");
+    ppl.complain("BE KIND");
+}
