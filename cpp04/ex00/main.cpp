f416f45eb1d2604120a9a8e91356a32ab41251f7
viforget <victor.forget99@gmail.com>
2021-11-24 15:29:54 +0100

    Add Wrong Animals

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -1,19 +1,39 @@
 #include "Animal.class.hpp"
 #include "Cat.class.hpp"
 #include "Dog.class.hpp"
+#include "WrongAnimal.class.hpp"
+#include "WrongCat.class.hpp"
+#include "WrongDog.class.hpp"
 
 int main()
 {
-	const Animal* animal = new Animal();
-	const Animal* cat = new Cat();
-	const Animal* dog = new Dog();
+	{
+		const Animal* animal = new Animal();
+		const Animal* cat = new Cat();
+		const Animal* dog = new Dog();
 
-	std::cout << animal->getType() << " " << std::endl;
-	std::cout << cat->getType() << " " << std::endl;
-	std::cout << dog->getType() << " " << std::endl;
-	animal->makeSound();
-	cat->makeSound();
-	dog->makeSound();
-	delete cat;
-	delete animal;
+		std::cout << animal->getType() << " " << std::endl;
+		std::cout << cat->getType() << " " << std::endl;
+		std::cout << dog->getType() << " " << std::endl;
+		animal->makeSound();
+		cat->makeSound();
+		dog->makeSound();
+		delete cat;
+		delete animal;
+	}
+	std::cout << "-------------------" << std::endl;
+	{
+		const WrongAnimal* animal = new WrongAnimal();
+		const WrongAnimal* cat = new WrongCat();
+		const WrongAnimal* dog = new WrongDog();
+
+		std::cout << animal->getType() << " " << std::endl;
+		std::cout << cat->getType() << " " << std::endl;
+		std::cout << dog->getType() << " " << std::endl;
+		animal->makeSound();
+		cat->makeSound();
+		dog->makeSound();
+		delete cat;
+		delete animal;	
+	}
 }
