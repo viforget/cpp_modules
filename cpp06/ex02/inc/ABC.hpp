e8fed7b998178aee417efa7e4d730baac4f3408a
viforget <victor.forget99@gmail.com>
2021-12-15 19:16:59 +0100

    ex02

---
+++ cpp06/ex02/inc/ABC.hpp
@@ -0,0 +1,10 @@
+#ifndef ABC_HPP
+# define ABC_HPP
+
+#include "Base.hpp"
+
+class	A : public Base {};
+class	B : public Base {};
+class	C : public Base {};
+
+#endif
---
+++ cpp06/ex02/src/main.cpp
@@ -0,0 +1,69 @@
+#include "Base.hpp"
+#include "ABC.hpp"
+# include <iostream>
+# include <cstdlib>
+
+Base *	generate(void)
+{
+	int		r = 0;
+	Base	*ret;
+
+	srand(time(0));
+	r = rand() % 3;
+	switch (r)
+	{
+		case 0:
+			ret = new A;
+			break;
+		case 1:
+			ret = new B;
+			break;
+		case 2:
+			ret = new C;
+			break;
+	}
+	return (ret);
+}
+
+void	identify(Base * p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "WTF" << std::endl;
+	
+}
+
+void identify( Base & p)
+{
+	try
+	{
+		static_cast<void>(dynamic_cast<A &>(p));
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception & e) {}
+	try
+	{
+		static_cast<void>(dynamic_cast<B &>(p));
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception & e) {}
+	try
+	{
+		static_cast<void>(dynamic_cast<C &>(p));
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception & e) {}
+}
+
+int main()
+{
+	Base *	a;
+	a = generate();
+	identify(a);
+	identify(*a);
+}
