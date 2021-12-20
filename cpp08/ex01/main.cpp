72721edebe1176cd25b99a2e186675f77e4d01ad
Victor Forget <viforget@pop-os.localdomain>
2021-12-20 23:51:47 +0100

    better main

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,13 +2,23 @@
 
 int main()
 {
-	Span sp = Span(5);
+	{
+		Span sp = Span(5);
 	
-	sp.addNumber(5);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+		sp.addNumber(5);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+	{
+		Span sp = Span(10000);
+
+		for (int i = 0; i < 10000; i++)
+			sp.addNumber(i);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
 }
