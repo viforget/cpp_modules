265ef2d5c15e22bec15aa5a750297105a5236ad9
viforget <victor.forget99@gmail.com>
2021-12-20 11:55:56 +0100

    Better main

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -34,3 +34,28 @@ std::cout << "------------------" << std::endl;
 }
 	return 0;
 }
+
+// class Awesome
+// {
+// 	public:
+// 		Awesome(void) : _n(0) {}
+// 		Awesome( int n ) : _n( n ) {}
+// 		Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+// 		bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+// 		bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+// 		bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+// 		bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+// 		bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+// 		bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+// 		int get_n() const { return _n; }
+// 	private:
+// 		int _n;
+// };
+// std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+// int main(void)
+// {
+// 	Awesome a(2), b(4);
+// 	swap(a, b);
+// 	std::cout << a << " " << b << std::endl; std::cout << max(a, b) << std::endl; std::cout << min(a, b) << std::endl; return (0);
+// }
